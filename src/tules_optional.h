c2883eeab52c2aed1424a1b70feb83f35657802e
esantoul <e.santoul@gmail.com>
2020-08-08 02:59:02 +0200

    Large Optional Rework  - Construction of mValue only if the optional contains data  - Added assignation methods & constructors  - Minimized contained object copies

--- src/tules_optional.h
+++ src/tules_optional.h
@@ -4,25 +4,58 @@
  * Optional and Optional Reference data container
  */
 
-namespace tules
+#include <new>
+
+namespace tules // Template Utility Library for Embedded Systems
 {
   template <typename T>
   class Optional
   {
   private:
-    T mValue;
-    bool mLive;
+    union {
+      T mValue;
+    };
+    bool mLive = false;
 
   public:
-    Optional() : mValue{}, mLive(false) {}
+    Optional() {}
     Optional(const T &val) : mValue{val}, mLive(true) {}
+    template <typename... Args>
+    explicit constexpr Optional(const Args &... args) : mValue{args...}, mLive(true) {}
+
+    Optional &operator=(const Optional &other)
+    {
+      if (mLive)
+        mValue.~T();
+      if (other.mLive)
+        new (&mValue) T(other.mValue);
+      mLive = other.mLive;
+      return *this;
+    }
+
+    template <typename O>
+    Optional &operator=(const O &val)
+    {
+      if (mLive)
+        mValue.~T();
+      new (&mValue) T(val);
+      mLive = true;
+      return *this;
+    }
+
+    ~Optional()
+    {
+      if (mLive)
+        mValue.~T();
+    }
 
     bool HasValue() { return mLive; }
     operator bool() { return mLive; }
     T Value() { return mValue; }
     T operator*() { return mValue; }
     T *operator->() { return &mValue; }
-    T ValueOr(const T &other) { return mLive ? Value() : other; }
+    T ValueOr(const T &other) { return mLive ? mValue : other; }
+    const T &ValueConstRefOr(const T &other) { return mLive ? mValue : other; }
   };
 
   template <typename T>
@@ -30,12 +63,13 @@ namespace tules
   {
   private:
     T *pValue;
-    bool mLive;
+    bool mLive = false;
 
   public:
-    OptionalRef() : pValue{nullptr}, mLive(false) {}
+    OptionalRef() {}
     OptionalRef(T &val) : pValue{&val}, mLive(true) {}
 
+    bool HasValue() { return mLive; }
     operator bool() { return mLive; }
     T &ValueRef() { return *pValue; }
     T &operator*() { return *pValue; }
@@ -44,16 +78,33 @@ namespace tules
 } // namespace tules
 
 /*
-#include <string>
+
+#include <cstdio>
+
+struct NoDefaultCtor
+{
+    int i;
+    NoDefaultCtor(int ii) : i{ii} {}
+    NoDefaultCtor(int ii, int ii2) : i{ii + ii2} {}
+    int val() const {return i;}
+    ~NoDefaultCtor() {printf("ndct dies %d\n", i);}
+};
+
 int main()
 {
   int ii = 10;
+
+  tules::Optional<NoDefaultCtor> op{};
+
   tules::OptionalRef<int> oor{ii};
   if (oor)
     oor.ValueRef() = 4;
 
-  std::string cc = "coucou";
-  tules::Optional<std::string> oo{cc};
-  return oo.ValueOr("oups").size() * ii;
+  tules::Optional<NoDefaultCtor> oo{6};
+  oo = 10;
+  if (oo)
+    return oo->val() * ii;
+  else
+    return NoDefaultCtor(4).val() * ii;
 }
 //*/
