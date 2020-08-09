88ec9a5523b8a8d3b1da1d0463b9f84b3a08fb89
esantoul <e.santoul@gmail.com>
2020-08-09 17:14:28 +0200

    Added Optional move ctor & assignment Added Doxygen style comments to Optional class & members

--- src/tules_optional.h
+++ src/tules_optional.h
@@ -1,13 +1,18 @@
 #pragma once
 
 /**
- * Optional and Optional Reference data container
+ * @file Optional and Optional Reference data container header file
+ * @author Etienne Santoul
  */
 
 #include <new>
+#include <utility>
 
 namespace tules // Template Utility Library for Embedded Systems
 {
+  /**
+   * @brief An optional container where the contained object is constructed only if explicitely needed
+   */
   template <typename T>
   class Optional
   {
@@ -18,11 +23,64 @@ namespace tules // Template Utility Library for Embedded Systems
     bool mLive = false;
 
   public:
-    Optional() {}
-    Optional(const T &val) : mValue{val}, mLive(true) {}
-    template <typename... Args>
-    explicit constexpr Optional(const Args &... args) : mValue{args...}, mLive(true) {}
+    /**
+     * @brief Empty constructor
+     */
+    Optional()
+    {
+    }
 
+    /**
+     * @brief Copy constructor
+     * @param other an Optional<T> lvalue
+     */
+    Optional(const Optional &other)
+    {
+      if (other.mLive)
+        new (&mValue) T(other.mValue);
+      mLive = other.mLive;
+    }
+
+    /**
+     * @brief Move constructor
+     * @param other an Optional<T> rvalue
+     */
+    Optional(Optional &&other)
+    {
+      if (other.mLive)
+        new (&mValue) T(std::move(other.mValue));
+      mLive = other.mLive;
+    }
+
+    /**
+     * @brief Direct initializing constructor
+     * @param args a list of arguments to construct type T
+     */
+    template <typename... Args>
+    Optional(const Args &... args) : mValue{args...}, mLive(true)
+    {
+    }
+
+    /**
+     * @brief Move assignment
+     * @param other an Optional<T> rvalue
+     * @return a lvalue reference to *this
+     */
+    Optional &operator=(Optional &&other)
+    {
+      if (mLive)
+        mValue.~T();
+      if (other.mLive)
+        new (&mValue) T(std::move(other.mValue));
+      mLive = other.mLive;
+      return *this;
+    }
+
+    /**
+     * @brief Copy assignment
+     * @param other an Optional<T> lvalue
+     * @return a lvalue reference to *this
+     */
     Optional &operator=(const Optional &other)
     {
       if (mLive)
@@ -33,6 +91,11 @@ namespace tules // Template Utility Library for Embedded Systems
       return *this;
     }
 
+    /**
+     * @brief Direct initializing assignment
+     * @param val a value to construct type T
+     * @return a lvalue reference to *this
+     */
     template <typename O>
     Optional &operator=(const O &val)
     {
@@ -43,6 +106,9 @@ namespace tules // Template Utility Library for Embedded Systems
       return *this;
     }
 
+    /**
+     * @brief Destructor
+     */
     ~Optional()
     {
       if (mLive)
@@ -81,30 +147,31 @@ namespace tules // Template Utility Library for Embedded Systems
 
 #include <cstdio>
 
-struct NoDefaultCtor
+struct TObj
 {
-    int i;
-    NoDefaultCtor(int ii) : i{ii} {}
-    NoDefaultCtor(int ii, int ii2) : i{ii + ii2} {}
-    int val() const {return i;}
-    ~NoDefaultCtor() {printf("ndct dies %d\n", i);}
+  int i;
+  TObj(int ii) : i{ii} { printf("tobj 1 i ctor %d\n", i); }
+  TObj(int ii, int ii2) : i{ii + ii2} { printf("tobj 2 i ctor %d\n", i); }
+  TObj(const TObj &o) : i{o.i} { printf("tobj cpy %d\n", i); }
+  TObj(TObj &&o) : i{o.i} { printf("tobj move %d\n", i); }
+  int val() const { return i; }
+  ~TObj() { printf("tobj dies %d\n", i); }
 };
 
 int main()
 {
   int ii = 10;
 
-  tules::Optional<NoDefaultCtor> op{};
-
   tules::OptionalRef<int> oor{ii};
   if (oor)
     oor.ValueRef() = 4;
 
-  tules::Optional<NoDefaultCtor> oo{6};
-  oo = 10;
+  tules::Optional<TObj> oo{6};
+  tules::Optional<TObj> oo2{};
+  oo2 = tules::Optional<TObj>{5, 2};
   if (oo)
     return oo->val() * ii;
   else
-    return NoDefaultCtor(4).val() * ii;
+    return TObj(4).val() * ii;
 }
 //*/
